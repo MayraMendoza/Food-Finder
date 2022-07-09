7771a0ab3027d33ab4985cbc7c8be87895260abe
MayraMendoza <mayra601m@gmail.com>
2022-07-08 21:33:00 -0400

    updated critique and profile controllers. Added GET all, GET by id and post request

--- src/main/java/com/careerDevs/Food/Finder/controllers/CritiqueController.java
+++ src/main/java/com/careerDevs/Food/Finder/controllers/CritiqueController.java
@@ -1,4 +1,63 @@
 package com.careerDevs.Food.Finder.controllers;
 
+import com.careerDevs.Food.Finder.models.Critique;
+import com.careerDevs.Food.Finder.models.Profile;
+import com.careerDevs.Food.Finder.repositories.CritiqueRepository;
+import com.careerDevs.Food.Finder.repositories.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/critiques")
 public class CritiqueController {
+    @Autowired
+    private CritiqueRepository critiqueRepository;
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @GetMapping("/test")
+    public ResponseEntity<?> testRoute(){
+        return new ResponseEntity<>("critique route", HttpStatus.OK);
+    }
+
+    @PostMapping("/{profileId}")
+    public ResponseEntity<?> createCritique(@PathVariable Long profileId, @RequestBody Critique newCritique){
+        Profile profile = profileRepository.findById(profileId).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        newCritique.setProfile(profile);
+        Critique critique = critiqueRepository.save(newCritique);
+        return new ResponseEntity<>(critique, HttpStatus.CREATED);
+    }
+
+    //Get all Critiques
+    @GetMapping("/")
+    public ResponseEntity<?> getAllCritiques(){
+        List<Critique> critiques = critiqueRepository.findAll();
+        return new ResponseEntity<>(critiques, HttpStatus.OK);
+    }
+
+    // get critique by id
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCritiqueById(@PathVariable Long id){
+        Optional <Critique> maybeCritique =  critiqueRepository.findById(id);
+        if(maybeCritique.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(maybeCritique.get(), HttpStatus.OK);
+    }
+
+    //Get all critiques made by a specific profile
+    @GetMapping("/profile/{profileId}")
+    public ResponseEntity<List<Critique>> getCritiquesByprofile(@PathVariable Long profileId){
+        List<Critique> critiques = critiqueRepository.findAllByprofile_id(profileId);
+        return new ResponseEntity<>(critiques, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/careerDevs/Food/Finder/controllers/ProfileController.java
+++ src/main/java/com/careerDevs/Food/Finder/controllers/ProfileController.java
@@ -1,4 +1,52 @@
 package com.careerDevs.Food.Finder.controllers;
 
+import com.careerDevs.Food.Finder.models.Profile;
+import com.careerDevs.Food.Finder.repositories.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/profiles")
 public class ProfileController {
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @GetMapping("/test")
+    public ResponseEntity<?> testRoute(){
+        return new ResponseEntity<>("profile route", HttpStatus.OK);
+    }
+
+    //Create a user profile
+    @PostMapping("/")
+    public ResponseEntity<?> createProfile(@RequestBody Profile newProfile){
+        Profile profile =profileRepository.save(newProfile);
+        return new ResponseEntity<>(profile, HttpStatus.CREATED);
+    }
+
+    //get all profiles
+    @GetMapping("/")
+    public ResponseEntity<List<Profile>> getAllProfiles(){
+        List<Profile> profiles= profileRepository.findAll();
+        return new ResponseEntity<>(profiles, HttpStatus.OK);
+    }
+
+    // get profile by id
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProfileById( @PathVariable Long id){
+        Optional<Profile> profile = profileRepository.findById(id);
+        if(profile.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(profile.get(),HttpStatus.OK);
+
+
+
+    }
 }
