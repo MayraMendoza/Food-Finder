dd3fe7f291c6abecb95bf766d8ae2d40bd1edcd4
MayraMendoza <mayra601m@gmail.com>
2022-07-08 21:31:05 -0400

    Added critique repository

--- src/main/java/com/careerDevs/Food/Finder/repositories/CritiqueRepository.java
+++ src/main/java/com/careerDevs/Food/Finder/repositories/CritiqueRepository.java
@@ -3,6 +3,9 @@ package com.careerDevs.Food.Finder.repositories;
 import com.careerDevs.Food.Finder.models.Critique;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CritiqueRepository extends JpaRepository<Critique, Long> {
+    List<Critique> findAllByprofile_id(Long profile_id);
 
 }
