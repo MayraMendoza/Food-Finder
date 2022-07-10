108675c178d189242b8f44343866e7d1a9b48afb
MayraMendoza <mayra601m@gmail.com>
2022-07-09 23:33:47 -0400

    added test routes for both restaurant and menuItem controller.

--- src/main/java/com/careerDevs/Food/Finder/controllers/MenuItemController.java
+++ src/main/java/com/careerDevs/Food/Finder/controllers/MenuItemController.java
@@ -1,6 +1,6 @@
 package com.careerDevs.Food.Finder.controllers;
 
-import com.careerDevs.Food.Finder.repositories.RestaurantRepository;
+import com.careerDevs.Food.Finder.repositories.MenuItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,17 +8,17 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/api/restaurants")
-public class RestaurantController {
+@RequestMapping("/api/menuitems")
+public class MenuItemController {
+
     @Autowired
-    private RestaurantRepository restaurantRepository;
+    private MenuItemRepository menuItemRepository;
 
     @GetMapping("/test")
-    public ResponseEntity <?> testRoute(){
-        return new ResponseEntity<>("Restaurant test route", HttpStatus.OK);
+    public ResponseEntity<?> testRoute(){
+        return new ResponseEntity<>("MenuItem test route", HttpStatus.OK);
     }
 }
