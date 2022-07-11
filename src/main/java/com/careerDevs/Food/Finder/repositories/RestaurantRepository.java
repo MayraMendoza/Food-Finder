7ab8833c068ace53003e302e857638f5c544db7c
MayraMendoza <mayra601m@gmail.com>
2022-07-11 16:17:31 -0400

    Added queries that find by inserted field. find by id, restaurant name, zipcode, address, cuisine

--- src/main/java/com/careerDevs/Food/Finder/repositories/RestaurantRepository.java
+++ src/main/java/com/careerDevs/Food/Finder/repositories/RestaurantRepository.java
@@ -3,5 +3,17 @@ package com.careerDevs.Food.Finder.repositories;
 import com.careerDevs.Food.Finder.models.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+import java.util.List;
+@Transactional
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    List<Restaurant> findByName(String name);
+    List<Restaurant> findById(long id);
+    List<Restaurant> findByAddress(String address);
+    List<Restaurant> findByCity(String city);
+    List<Restaurant> findByState(String state);
+    List<Restaurant> findByZipcode(String zipcode);
+    List<Restaurant> findByPhoneNumber(String phoneNumber);
+    List<Restaurant> findByCuisine(String cuisine);
+
 }
