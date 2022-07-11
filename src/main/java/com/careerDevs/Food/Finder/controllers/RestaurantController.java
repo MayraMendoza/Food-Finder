933aa4efe35de46bdd759fe7ef0583d4eec3fa28
MayraMendoza <mayra601m@gmail.com>
2022-07-11 18:01:19 -0400

    Added delete mapping that deletes by inserted field. find by id, restaurant name, zipcode, address, cuisine

--- src/main/java/com/careerDevs/Food/Finder/controllers/RestaurantController.java
+++ src/main/java/com/careerDevs/Food/Finder/controllers/RestaurantController.java
@@ -2,14 +2,17 @@ package com.careerDevs.Food.Finder.controllers;
 
 import com.careerDevs.Food.Finder.models.Restaurant;
 import com.careerDevs.Food.Finder.repositories.RestaurantRepository;
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 @CrossOrigin
@@ -48,4 +51,99 @@ public class RestaurantController {
             }
             return new ResponseEntity<>(restaurant.get(), HttpStatus.OK);
     }
+
+    @GetMapping("/{field}/{value}")
+    private ResponseEntity<?> getOverviewByField(@PathVariable String field, @PathVariable String value){
+        try{
+            List<Restaurant> foundRestaurant = null;
+            field = field.toLowerCase();
+            switch (field){
+                case "name" -> foundRestaurant = restaurantRepository.findByName(value);
+                case "id" -> foundRestaurant = restaurantRepository.findById(Long.parseLong(value));
+                case "address" -> foundRestaurant = restaurantRepository.findByAddress(value);
+                case "city" -> foundRestaurant= restaurantRepository.findByCity(value);
+                case "state" -> foundRestaurant = restaurantRepository.findByState(value);
+                case "zipcode" -> foundRestaurant = restaurantRepository.findByZipcode(value);
+                case "phonenumber" -> foundRestaurant = restaurantRepository.findByPhoneNumber(value);
+                case "cuisine" -> foundRestaurant = restaurantRepository.findByCuisine(value);
+
+
+            }
+            if(foundRestaurant == null || foundRestaurant.isEmpty()){
+                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "did not match any " + field);
+            }
+            return ResponseEntity.ok(foundRestaurant);
+
+        } catch (NumberFormatException e){
+            return ResponseEntity.status(400).body("please enter a number");
+        }catch (HttpClientErrorException e){
+            return ResponseEntity.status(e.getStatusCode().value()).body(e.getMessage());
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println(e.getClass());
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+
+
+        }
+
+        //delete all
+    @DeleteMapping("/all")
+    public ResponseEntity<?> deleteAllFromDatabase(){
+        try{
+            //get total number of restaurant
+            long totalRestaurants = restaurantRepository.count();
+            if(totalRestaurants==0){
+                return ResponseEntity.ok("There are no restaurants to delete");
+            }
+            restaurantRepository.deleteAll();
+            return ResponseEntity.ok("deleted Overviews" + totalRestaurants);
+        } catch (HttpClientErrorException e){
+            return ResponseEntity.status(e.getStatusCode().value()).body(e.getMessage());
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println(e.getClass());
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    // delete
+    @DeleteMapping("/{filed}/{value}")
+    private ResponseEntity<?> getRestaurantByField(@PathVariable String field, @PathVariable String value){
+        try{
+            List<Restaurant> foundRestaurant = null;
+            field = field.toLowerCase();
+            switch (field){
+                case "id" -> foundRestaurant = restaurantRepository.findById(Long.parseLong(value));
+                case "name" -> foundRestaurant = restaurantRepository.findByName(value);
+                case "address" -> foundRestaurant = restaurantRepository.findByAddress(value);
+                case "city" -> foundRestaurant = restaurantRepository.findByCity(value);
+                case "state" -> foundRestaurant =restaurantRepository.findByState(value);
+                case "zipcode" -> foundRestaurant =restaurantRepository.findByZipcode(value);
+                case "phoneNumber" -> foundRestaurant = restaurantRepository.findByPhoneNumber(value);
+                case "cuisine" -> foundRestaurant = restaurantRepository.findByCuisine(value);
+
+            }
+            if(foundRestaurant == null || foundRestaurant.isEmpty()){
+                throw  new HttpClientErrorException(HttpStatus.BAD_REQUEST, "did not match any" + field);
+            }
+            return ResponseEntity.ok(foundRestaurant);
+
+        } catch (NumberFormatException e){
+            return ResponseEntity.status(400).body("please enter a number");
+        }catch (HttpClientErrorException e){
+            return ResponseEntity.status(e.getStatusCode().value()).body(e.getMessage());
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println(e.getClass());
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+
+    }
+
+
+
+
+
 }
--- src/main/java/com/careerDevs/Food/Finder/repositories/RestaurantRepository.java
+++ src/main/java/com/careerDevs/Food/Finder/repositories/RestaurantRepository.java
@@ -16,4 +16,14 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     List<Restaurant> findByPhoneNumber(String phoneNumber);
     List<Restaurant> findByCuisine(String cuisine);
 
+    List<Restaurant> deleteByName(String name);
+    List<Restaurant> deleteById(long id);
+    List<Restaurant> deleteByAddress(String address);
+    List<Restaurant> deleteByCity(String city);
+    List<Restaurant> deleteByState(String state);
+    List<Restaurant> deleteByZipcode(String zipcode);
+    List<Restaurant> deleteByPhoneNumber(String phoneNumber);
+    List<Restaurant> deleteByCuisine(String cuisine);
+
+
 }
